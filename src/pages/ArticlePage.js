452542e098f9ff60490d28025be981b7112ada54
Sumit <sk7627577@gmail.com>
2023-01-10 21:00:12 +0530

    add axios & use state to artpg & li-no 16 setart*

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import articles from './article-content';
+import { useState,useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import { NotFoundPage } from './NotFoundPage';
+import articles from './article-content';
 
 export const ArticlePage = () => {
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: []});
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const response = await axios.get(`http://localhost:8000/api/articles/${articleId}`);
+    const newArticleInfo = response.data;
+    setArticleInfo({upvotes: Math.ceil(Math.random()*10)+1, comments: []});
+  },[]);
   const article = articles.find(article => article.name === articleId);
   if (!article) {
     return <NotFoundPage />
@@ -13,6 +23,7 @@ export const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)!</p>
       {article.content.map((paragraph, i)=> (<p key={i}>{paragraph}</p>))}
     </>
   )
