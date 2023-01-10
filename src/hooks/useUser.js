d89f41e23a73455b9b477ec07f94b6580e5658f9
Sumit <sk7627577@gmail.com>
2023-01-11 01:16:12 +0530

    login pg i create & sync

---
+++ src/hooks/useUser.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged} from 'firebase/auth'
+
+export const useUser = () => {
+    const [user, setUser] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect (() => {
+        const unSu
+    })
+  return (
+    <div>useUser</div>
+  )
+}
